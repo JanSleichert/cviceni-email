ba0b79dc1180114c99cf6796f635acdc10e7a5a1
Jan Šleichert <jan.sleichert@gmail.com>
2023-11-16 19:55:41 +0100

    splnění zadání

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+const goodbye = (jmeno) => {
+    const uzaver = document.querySelector(".email__closing")
+    uzaver.textContent = `Na shledanou ${jmeno}`
+
+}
+const funkce = goodbye(`Pavel Ovesný`)
 
